f87d1bb0db061dae80566d0a82c0eb2025a13b01
Jakub Frelik <git@jfrelik.dev>
2024-03-13 19:58:49 +0100

    feat: add calculateAvg method

--- apbd-1/apbd-1/Program.cs
+++ apbd-1/apbd-1/Program.cs
@@ -5,10 +5,20 @@ public class Program
     public static void Main(string[] args)
     {
         int g = 10;
-        
+
         Console.WriteLine("Hello world");
-        
+
         Console.WriteLine("Buenos dias");
-        
+    }
+
+    static int calculateAvg(int[] tab)
+    {
+        int avg = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            avg += tab[i];
+        }
+
+        return avg / tab.Length;
     }
 }
